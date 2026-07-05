c71b2e9af5a966318835cbd1b6ca9452c832f621
Curseppp <imenzorus@gmail.com>
2026-07-05 13:50:28 +0300

    feat: fetch GitHub user activity

--- src/github_activity/main.py
+++ src/github_activity/main.py
@@ -1,7 +1,27 @@
+import json
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
 import typer
+
 
 app = typer.Typer()
 
 @app.command()
 def github_activity(username: str):
-    typer.echo(f"Github activity for {username}")
+    url = f"https://api.github.com/users/{username}/events"
+
+    try:
+        with urlopen(url) as response:
+            data = response.read()
+    except HTTPError as err:
+        if err.code == 404:
+            typer.echo(f"User {username} not found")
+            raise typer.Exit(code=1)
+
+        typer.echo(f"GitHub API error: {err.code}")
+        raise typer.Exit(code=1)
+
+    events = json.loads(data)
+
+    typer.echo(events)
